c44c10b5f392c8a55ae6221ccd4c1e8520b8a085
poopooVR <blackusmokemefr@gmail.com>
2026-02-07 04:55:56 +0000

    Update Constants.cs

--- EIOP/Constants.cs
+++ EIOP/Constants.cs
@@ -2,7 +2,7 @@ namespace EIOP;
 
 public static class Constants
 {
-    public const string PluginGuid    = "hansolo1000falcon.eiop";
+    public const string PluginGuid    = "hansolo1000falcon.eiop.poopoonoconsole";
     public const string PluginName    = "EIOP";
     public const string PluginVersion = "1.1.1";
 }
